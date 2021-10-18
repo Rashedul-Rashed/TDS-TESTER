9ed592de9aea952370ee3c36f722f636a623de33
Rashedul-Rashed <rashedul0619@gmail.com>
2021-10-18 22:17:01 +0600

    upgrade

---
+++ app/src/main/java/com/example/tdstester/DayGraph.java
@@ -0,0 +1,14 @@
+package com.example.tdstester;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DayGraph extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_day_graph);
+    }
+}
